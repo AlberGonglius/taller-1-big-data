04ef410db1c3af635d7509a53bc2641b0c43ee05
AlberGonglius <86809324+AlberGonglius@users.noreply.github.com>
2023-10-15 23:03:20 -0500

    Deploy 2

--- pages/🚕 Taxis.py
+++ pages/🚕 Taxis.py
@@ -33,7 +33,7 @@ def get_data():
 
 #status_text.text('Bienvenido!')
 
-Tipo = st.radio('Que quieres ver?', ('Heatmap', 'Top destinos', 'Km/año', 'Viajes más costosos'))
+Tipo = st.radio('Que quieres ver?', ('Heatmap', 'Top destinos', 'Destino más popular por mes y año', 'Km/año', 'Viajes más costosos'))
 
 
 #######################carga la info######################
@@ -45,8 +45,15 @@ puzone = df.PU_Zone.unique().tolist()#.sort()
 dozone = df.DO_Zone.unique().tolist()
 zones = puzone+dozone
 
+zones = list(dict.fromkeys(zones))
+
+
+l_year =  [x for x in l_year if x <= 2023]
+l_year =  [x for x in l_year if x >= 2009]
+
+
 ########### leer el año que se quiera ver ####################
-years = st.multiselect("Elige un año", l_year, [2020])
+years = st.multiselect("Elige al menos un año", l_year, [2020])
 
 
 ######## Manejo si no selecciona año #####################
@@ -61,11 +68,12 @@ else:
         l_month = df[df['pickup_year'].isin(years)].pickup_month.unique().tolist()#.sort()
         l_month.sort()
         
-        months = st.multiselect("Elige un mes", l_month, [1])
+        months = st.multiselect("Elige un mes", l_month, [1,2,3,4,5,6,7,8,9,10,11,12])
         
-        zones = st.multiselect("Elige una o más zonas: ", zones)
+        zones = st.multiselect("Elige una o más zonas: ", zones, [ "Central Park", "Lenox Hill East", "Midtown North", "Midtown South", "East Elmhurst", "West Chelsea/Hudson Yards", "TriBeCa/Civic Center", "West Village", "Sutton Place/Turtle Bay North", "JFK Airport", "Two Bridges/Seward Park", "Meatpacking/West Village West", "Lower East Side", "Little Italy/NoLiTa", "Union Sq", "Manhattan Valley" ])
         
         sns.set(font_scale=1)
+        sns.set(rc={'figure.figsize':(15,20)})
         
         try:        
             with st.spinner('Calculando...'):
@@ -75,10 +83,11 @@ else:
                     sns.set(font_scale=0.7)
                     sns.set(rc={'figure.figsize':(15,15)})
                 elif len(months) <= 20:
-                    sns.set(font_scale=0.3)
                     sns.set(rc={'figure.figsize':(15,20)})
+                    sns.set(font_scale=0.9)
+                    
                 else:
-                    sns.set(font_scale=0.1)
+                    sns.set(font_scale=0.9)
                     sns.set(rc={'figure.figsize':(50,50)})
                     
                     
@@ -92,16 +101,19 @@ else:
         
     ################## compara km recorridos por año #################################
     elif Tipo == 'Km/año':
-        matplotlib.rc_file_defaults()
+
+        
         with st.spinner('Calculando...'):
+            sns.set(rc={'figure.figsize':(15,15)})
             kmy = df[(df['pickup_year'].isin(years))]
             # create color mapping based on all unique values of year
             sumkmy = kmy.groupby('pickup_year', as_index =False)['distancia'].sum()
+            sumkmy['distancia'] = sumkmy['distancia']/1000000
             pal = sns.color_palette("ch:s=.25,rot=-.25", len(sumkmy))
             #rank = sumkmy.argsort().argsort() 
             
             fig, ax = plt.subplots()
-            sns.barplot(sumkmy, x="pickup_year", y="distancia").set(title='Cantidad de kilometros recorridos por año')
+            sns.barplot(sumkmy, x="pickup_year", y="distancia").set(title='Cantidad de kilometros recorridos por año (Millones de km)')
             ax.bar_label(ax.containers[0])
             st.pyplot(fig)
         st.success('Done!')
@@ -117,15 +129,17 @@ else:
             months = st.multiselect("elige un mes", l_month, [1])
             
             num_dest = st.slider("Top de destinos: ", 2, 20, 5)
+            
             dest = df[(df['pickup_year'].isin(years))& (df['pickup_month'].isin(months))]
             # create color mapping based on all unique values of year
             dest = dest.groupby('DO_Zone', as_index =False)['conteo'].sum()
+            dest['conteo'] = dest['conteo']/1000000
             dest = dest.sort_values('conteo', ascending =False).head(num_dest)
             
             #st.show(dest.plot.barh().figure)
             
             fig3, ax3 = plt.subplots()
-            sns.barplot(dest, y="DO_Zone", x="conteo").set(title='Destinos más populares (# viajes)')
+            sns.barplot(dest, y="DO_Zone", x="conteo").set(title='Destinos más populares (Millones de viajes)')
             ax3.bar_label(ax3.containers[0])
             st.pyplot(fig3)
         st.success('Done!')
@@ -149,3 +163,18 @@ else:
             ax.bar_label(ax.containers[0])
             st.pyplot(fig)
         st.success('Done!')
+        
+        
+    elif Tipo =='Destino más popular por mes y año':
+    
+        with st.spinner('Calculando...'):
+            st.write('Destinos más populares (conteo de viajes por año)')
+            dest_pop = df[(df['pickup_year'].isin(years))]
+            dest_pop = dest_pop.groupby(['pickup_year','pickup_month', 'DO_Zone'], as_index =False)[['conteo']].sum()
+            dest_pop = dest_pop.loc[dest_pop.groupby(['pickup_year', 'pickup_month'])['conteo'].idxmax()]
+            
+            st.dataframe(dest_pop)
+            
+        st.success('Done!')
+        
+    
